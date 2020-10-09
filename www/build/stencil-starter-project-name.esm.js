73fd6566e2c711be310b45018cb4e0af3148d643
Robert Axelsen <robert.axelsen@ebcont.com>
2020-10-09 08:03:16 +0200

    remove www from gitignore to make it easy to deploy

---
+++ www/build/stencil-starter-project-name.esm.js
@@ -0,0 +1,1 @@
+import{p as o,b as p}from"./p-df55f100.js";(()=>{const p=import.meta.url,t={};return""!==p&&(t.resourcesUrl=new URL(".",p).href),o(t)})().then(o=>p([["p-fa99749b",[[1,"my-component",{data:[1]}]]]],o));
