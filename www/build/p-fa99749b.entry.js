44c72144a80a5015455278817aa1f26b62a56479
Robert Axelsen <robert.axelsen@ebcont.com>
2020-10-09 08:07:44 +0200

    revert gitignore change

--- www/build/p-fa99749b.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as s,h as o}from"./p-df55f100.js";const r=class{constructor(o){s(this,o)}render(){return o("div",null,JSON.stringify(this.data))}};r.style=":host{display:block}";export{r as my_component}
